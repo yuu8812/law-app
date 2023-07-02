68195506f5ba9a925434b03f93f0c01f6aa3ba4d
yuu8812 <foir075@gmail.com>
2023-07-03 00:05:18 +0900

    second commit

--- hosting/src/app/law/[lawDetail]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-interface Props {}
-
-const loading: FC<Props> = (props): JSX.Element => {
-  return <>loading</>;
-};
-
-export default loading;
--- hosting/src/app/law/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-interface Props {}
-
-const page: FC<Props> = (props): JSX.Element => {
-  return <>page</>;
-};
-
-export default page;
